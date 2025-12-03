ab915130ad26bdb9a5ddbdd7a36b943d9bb56b4f
Júlia Coité <119825956+juliacoit@users.noreply.github.com>
2025-12-03 15:20:33 -0300

    Refactor logical functions and include headers

--- Julia.c
+++ Julia.c
@@ -1,3 +1,7 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "Julia.h"
+
 int logico_or (int a, int b){
     if (A + B >= 1) return 1;
     else return 0;
@@ -6,3 +10,4 @@ int logico_nor (int a, int b){
     if (A + B == 0) return 1;
     else return 0;
 }
+
