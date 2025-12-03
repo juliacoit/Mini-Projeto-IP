8665cdbd99226c1829ea2604e1bc86cd8dfd2dad
Júlia <juliascoite@gmail.com>
2025-12-03 16:24:36 -0300

    configurando ambiente, projeto rodando

--- Julia.c
+++ Julia.c
@@ -3,11 +3,11 @@
 #include "Julia.h"
 
 int logico_or (int a, int b){
-    if (A + B >= 1) return 1;
+    if (a + b >= 1) return 1;
     else return 0;
 }
 int logico_nor (int a, int b){
-    if (A + B == 0) return 1;
+    if (a + b == 0) return 1;
     else return 0;
 }
 
