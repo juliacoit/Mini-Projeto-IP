fb1f5d8637f2a3540de6cffc91f516f474e6603c
Aryan Douglas Borges Rodrigues <aryandouglas14@gmail.com>
2025-12-03 21:18:01 -0300

    Add header file Aryan.h with bitwise functions

---
+++ Aryan.h
@@ -0,0 +1,10 @@
+#ifndef ARYAN.H
+#define ARYAN.H
+
+int Desloca_Bits_Direita(int valor, int bits);
+int Desloca_Bits_Esquerda(int valor, int bits);
+int Xor_Bitwise(int valor, int bits);
+int Or_Bitwise(int valor, int bits);
+int And_Bitwise(int valor, int bits);
+
+#endif
